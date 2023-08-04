8b7399e866f5b57f98ef763583d609fe03c512d7
Francesco <fradeca01@gmail.com>
2023-08-04 13:25:08 +0200

    work on R analysis

--- R/r.R
+++ R/r.R
@@ -4,7 +4,7 @@ require("stringr")
 require("dplyr")
 require("ggplot2")
 require("lubridate")
-
+require("modelr")
 
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "progetto_basi",
@@ -61,8 +61,8 @@ fornitori %>%
 
 #4
 
-dipendenti_età = dbGetQuery(con, "select dipendente.matricola, progetto, data_di_nascita 
-from coinvolge inner join dipendente on dipendente.matricola = coinvolge.matricola;
+dipendenti_età = dbGetQuery(con, "select dipendente.matricola, progetto, data_di_nascita, progetto.budget 
+from coinvolge inner join dipendente on dipendente.matricola = coinvolge.matricola inner join progetto on progetto.codice_aziendale = coinvolge.progetto;
 ")
 
 dipendenti_età = tibble(dipendenti_età)
@@ -71,24 +71,49 @@ dipendenti_età = tibble(dipendenti_età)
 birth_date <- as.Date("1973-01-01")
 now = Sys.Date()
 
+collocate = function(budget) {
+    case_when(
+        budget < 10000 ~ 1,
+        budget < 20000 ~ 2,
+        budget < 30000 ~ 3,
+        budget < 40000 ~ 4,
+        TRUE ~ 5
+    )
+}
+
 dipendenti_età %>%
     mutate(age = trunc((data_di_nascita %--% now) /years(1))) %>%
-    group_by(progetto) %>%
+    mutate(fascia_progetto = collocate(budget)) %>%
+    group_by(fascia_progetto) %>%
     summarise(eta_media = mean(age)) %>%
     arrange(desc(eta_media)) %>%
     top_n(10) %>%
-    mutate(progetto = as.character(progetto)) %>%
-    ggplot(aes(x = reorder(progetto, -eta_media), y = eta_media)) + 
+    ggplot(aes(x = reorder(fascia_progetto, fascia_progetto), y = eta_media)) + 
     geom_col(fill="red") + 
-    labs(title = "Età media per progetto", x = "Progetto", y = "Età media") + 
+    labs(title = "Età media per fascia progetto", x = "Fascia Progetto", y = "Età media") + 
     theme_bw()
 
 
 
 #5
 
-num_budget = dbGetQuery("select progetto, count(*), budget 
+num_budget = dbGetQuery(con, "select progetto, count(*) as num_dip, budget 
 from coinvolge inner join progetto on coinvolge.progetto = progetto.codice_aziendale
  group by progetto, budget;")
 
+num_budget = tibble(num_budget)
 
+num_budget %>%
+    ggplot(aes(x = num_dip, y = budget)) +
+    geom_point()
+
+model = lm(data = num_budget, num_dip ~ budget)
+
+correlation = cor(num_budget$num_dip, num_budget$budget)
+
+num_budget = add_predictions(num_budget, model)
+
+num_budget %>%
+    ggplot() +
+    geom_point(aes(x = num_dip, y = budget)) + 
+    geom_line(aes(x = num_dip, y = pred))
